6fd6dea659973c272cdf75d5dd06fd577630640b
Dhirendra10251 <dhirendra.25bai10251@vitbhopal.ac.in>
2025-12-13 09:11:14 +0000

    created a folder for all array questions and solved array sort check

---
+++ arrays/array_sort_check.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class solution{
+public:
+    bool check(vector<int>& nums){
+        int n = nums.size();
+        int dips = 0;
+
+        for(int i = 1;i<n;i++){
+            if(nums[i-1]>nums[i]){ 
+                dips++;
+            }
+            if(nums[n-1]>nums[0]){
+                dips++;
+            }
+        }
+        return dips <= 1;
+    }
+};

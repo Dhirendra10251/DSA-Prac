3801ddd7b1011ba4fbc39a8f978578331f2ad672
Dhirendra10251 <dhirendra.25bai10251@vitbhopal.ac.in>
2025-12-14 18:06:35 +0000

    added the optimal solution of rightrotate_kth_term

--- arrays/rightrotate_by_kth_term.cpp
+++ arrays/rightrotate_by_kth_term.cpp
@@ -3,11 +3,11 @@ using namespace std;
 //bruteforce
 class solution1{
 public:
-    void rotate(vector<int>& nums, int k){
+    void rotate1(vector<int>& nums, int k){
     int n = nums.size();
     k = k % n; 
     int temp[n]; //insering elements in temprorary variable
-    for(int i=0;i<n;i++){
+    for(int i=0;i<k;i++){
         temp[i] = nums[i];
     }
     for(int i=n-k;i<n;i++){
@@ -16,6 +16,18 @@ public:
     for(int i=k;i<n;i++){
         nums[i] = temp[i-k];
     }
+    return;
+    }
+};
+//optimal soln - better space complexity but a little incresed TC.
+class solution2{
+public:
+    void rotate2(vector<int>& arr, int d){
+    int n = arr.size();
+    d = d % n;
+    reverse(arr.begin(),arr.begin()+n);
+    reverse(arr.begin(),arr.begin()+d);
+    reverse(arr.begin()+d,arr.begin()+n);
     return;
     }
 };

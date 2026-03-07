17f8f532235bb1f8e2e686e2e44660bf441e21c7
Dhirendra10251 <dhirendra.25bai10251@vitbhopal.ac.in>
2026-03-07 06:57:06 +0000

    added optimal solution

--- arrays/longest_subarray_withSum_k.cpp
+++ arrays/longest_subarray_withSum_k.cpp
@@ -22,3 +22,27 @@ public:
         return max_len;
     }  
 };
+
+//optimal solution(using two pointer)
+class solution{
+public:
+    int findLongestSubArrayWithSumK(vector<int>& nums,long long k){
+    int left =0, right =0;
+    long long sum = nums[0];
+    int max_len = 0;
+    while(right<nums.size()){
+        while(left<=right && sum==k){
+            sum -= nums[left];
+            left++;
+        }
+        if(sum==k){
+            max_len=max(max_len,right-left+1);
+        }
+        right++;
+        if(right<nums.size()){
+            sum += nums[right];
+        }
+    }
+    return max_len;
+    }  
+};
